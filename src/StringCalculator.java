10a9015dba4681adabd425bf5ca3c60597334438
hanbi <rain7i@naver.com>
2020-11-09 22:59:42 +0900

    [refactor] 빈 문자열 또는 null 입력시 0 반환, 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -3,18 +3,39 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
-    int add(String input) {
-        int result = 0;
-        if (input==null||input.isEmpty()) {
-            return result;
-        }
-        if (input.contains(",")){
-            String[] values = input.split(",");
-            for(String val : values){
-                result += Integer.parseInt(val);
-            }
-            return result;
-        }
-        return Integer.parseInt(input);
+    public int add(String input) {
+        if (isBlank(input)) return 0;
+        return sum(toInteger(input.split(",")));
     }
+
+    /**
+     * Check if input is Blank or not
+     */
+    private Boolean isBlank(String input) {
+        if (input == null || input.isEmpty()) return true;
+        return false;
+    }
+
+    /**
+     * String to Int
+     */
+    private int[] toInteger(String[] values) {
+        int[] nums = new int[values.length];
+        for (int i = 0; i < values.length; i++) {
+            nums[i] = Integer.parseInt(values[i]);
+        }
+        return nums;
+    }
+
+    /**
+     * 합"만" 구해야 함
+     */
+    private int sum(int[] values) {
+        int result = 0;
+        for (int val : values) {
+            result += val;
+        }
+        return result;
+    }
+
 }
